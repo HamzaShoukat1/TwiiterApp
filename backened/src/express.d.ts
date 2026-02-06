b2b9b2fbdc5530c09adb96a01917741415240620
hamza <=>
2026-02-06 18:35:33 +0500

    a

--- backened/src/express.d.ts
+++ backened/src/express.d.ts
@@ -1,9 +1,11 @@
 import { IUser } from "./Types/Model.Types.ts"
-import "express-serve-static-core";
 
 
-declare module "express-serve-static-core" {
-  interface Request {
-    user?: IUser;
-  }
-}
+
+declare global {
+    namespace Express {
+        interface Request {
+            user: IUser
+        }
+    }
+};
